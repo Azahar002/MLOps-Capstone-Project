7c677a016f6de069b789c87817b2b06a8d09f0bb
Azahar <Azahar@ml.com>
2025-04-28 23:39:41 -0500

    CI/CD run-3

--- src/model/register_model.py
+++ src/model/register_model.py
@@ -32,8 +32,8 @@ mlflow.set_tracking_uri(f'{dagshub_url}/{repo_owner}/{repo_name}.mlflow')
 
 # Below code block is for local use
 # -------------------------------------------------------------------------------------
-mlflow.set_tracking_uri('https://dagshub.com/Azahar002/MLOps-Capstone-Project.mlflow')
-dagshub.init(repo_owner='Azahar002', repo_name='MLOps-Capstone-Project', mlflow=True)
+# mlflow.set_tracking_uri('https://dagshub.com/Azahar002/MLOps-Capstone-Project.mlflow')
+# dagshub.init(repo_owner='Azahar002', repo_name='MLOps-Capstone-Project', mlflow=True)
 # -------------------------------------------------------------------------------------
 
 
